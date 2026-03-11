ebe7b485172ce200229b10178752182b83c42ed9
sergei <serbogomya2022@gmail.com>
2026-03-11 18:53:12 +0300

    когда уже заработает

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,27 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
-  base: '/',
-  root: '.',
+  base: '/coffeShop/',
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'index.html'),
+        menu: resolve(__dirname, 'menu.html'),
+        cart: resolve(__dirname, 'cart.html'),
+        login: resolve(__dirname, 'login.html'),
+        about: resolve(__dirname, 'about.html'),
+        contact: resolve(__dirname, 'contact.html')
+      }
+    },
+    outDir: 'dist'
+  },
   server: {
     proxy: {
-      '/api': { 
+      '/api': {
         target: 'http://localhost:3000',
         changeOrigin: true
       }
     }
   }
-})
+});
